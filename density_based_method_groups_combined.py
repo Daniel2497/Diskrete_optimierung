7c7ec78779a6d6589e7bbdf7e23d97170e314300
Daniel Emil Giring <daniel.giring@uni-jena.de>
2021-11-03 10:52:40 +0100

    wie alt

--- density_based_method_groups_combined.py
+++ density_based_method_groups_combined.py
@@ -14,7 +14,7 @@ from sklearn.cluster import KMeans
 #Lösungvorschlag, x3 zu mehren Gruppen zuweisen und am Ende die Gruppe
 #miteinander vergleichen
 
-N=250
+N=50
 X, y = make_blobs(n_samples=N, centers=4,
                    random_state=42)#, cluster.std=0.7)
 print(X.shape)
@@ -85,6 +85,13 @@ for i in range(N):
             isolatedpoints_scnd_order.append(i)
         else:
             neighbor_and_corepoints.append(closest_couple)
+for a1 in corepoints:
+    for a2 in corepoints:
+        for s1 in a1:
+            for s2 in a2:
+                if ((X[s1,0]-X[s2,0])**2+(X[s1,1]-X[s2,1])**2)<epsilon:
+                    a1+=a2
+                    #corepoints.remove(list(a2))
 
 
 
@@ -93,23 +100,23 @@ print(neighbor_and_corepoints)
 colarr=["aliceblue","azure","black","white","blueviolet","brown","gray","yellow","gold","green"]
 collcounter=0
 #Markiere Corepoints
-for a in corepoints:
-    col= ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])]#Farben zufällig wählen
-    for s in a:        
+#for a in corepoints:
+#    col= ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])]#Farben zufällig wählen
+#    for s in a:        
         #plt.scatter(X[s,0],X[s,1],marker="P",color=colarr[collcounter%10])
-        plt.scatter(X[s,0],X[s,1],marker="P",color=col)
+#        plt.scatter(X[s,0],X[s,1],marker="P",color=col)
         #Markiere Nachbar von Corepoint, welche kein Corepoint ist
-        for couple in neighbor_and_corepoints:
-            if couple[1]==s:
+#        for couple in neighbor_and_corepoints:
+#            if couple[1]==s:
                 #plt.scatter(X[couple[0],0],X[couple[0],1],marker="x",color=colarr[collcounter%10])
-                plt.scatter(X[couple[0],0],X[couple[0],1],marker="x",color=col)
+#               plt.scatter(X[couple[0],0],X[couple[0],1],marker="x",color=col)
     #collcounter=collcounter+1
 #Markieren Isolatedpoints
-for s in isolatedpoints:
-    plt.scatter(X[s,0],X[s,1],marker="x",color="red")
+#for s in isolatedpoints:
+#    plt.scatter(X[s,0],X[s,1],marker="x",color="red")
 #Markiere Isolatedpoints scnd order
-for s in isolatedpoints_scnd_order:
-    plt.scatter(X[s,0],X[s,1],marker="P",color="red")
+#for s in isolatedpoints_scnd_order:
+#    plt.scatter(X[s,0],X[s,1],marker="P",color="red")
 #print(X[i,1],X[i,0])
 print("Die Anzahl der verschiedenen Gruppen von Corepoints beträgt: ",len(corepoints))
 corenumber=0
@@ -121,4 +128,4 @@ print(corenumber)
 #print(model.cluster_centers_)
 #centers = np.array(model.cluster_centers_)
 #plt.scatter(centers[:,0], centers[:,1], marker="x", color='r')
-plt.show()
+#plt.show()
